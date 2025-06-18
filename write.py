1ebc2368742f5544a66f0cd6f13a05e9852116b6
Samuel Colvin <s@muelcolvin.com>
2025-06-18 09:50:51 +0100

    initial commit

---
+++ write.py
@@ -0,0 +1,7 @@
+import csv
+
+with open("data.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["a", "b"])
+    for i in range(1, 10_000_000):
+        writer.writerow([i, i + 201])
